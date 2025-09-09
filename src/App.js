9c7c4e2585806bbecf3a176a7d584dda141e9043
Diksha Sharma <baswalganesh1@gmai.com>
2025-09-09 11:18:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import { Slab } from 'react-loading-indicators';
+import './App.css';
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='pixel'>
+      <p>THIS WEBSITE IS UNDER CONSTRUCTION</p>
+      <Slab color="#2c94e5" size="medium" text="" textColor="" />
+      <br/>
+      <p>Check out my work  :</p>
+      
+      
+        <a href='https://filminfosis.netlify.app/'>Filminfosis</a>
+        
+        <a href='https://bswl-todolist.netlify.app/'>TODO-app</a>
+      
+      
+    </div>
+  )
+}
