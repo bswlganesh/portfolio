804e93117ce14d79540e83a50739c28438b869dc
Baswal Ganesh <baswalganesh1@gmail.com>
2025-09-11 13:11:30 +0530

    added new css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-
-import { Slab } from 'react-loading-indicators';
-import './App.css';
-
-import React from 'react'
+import { Slab } from "react-loading-indicators";
+import "./App.css";
+import React from "react";
 
 export default function App() {
   return (
-    <div className='pixel'>
-      <p>bswlganesh</p>
-      <p>This Website is under Construction</p>
-      <Slab color="#2c94e5" size="medium" text="" textColor="" />
-      
-      <p>Check out my work  :</p>
-      
-      
-        <a href='https://filminfosis.netlify.app/'>Filminfosis</a>
-        
-        <a href='https://bswl-todolist.netlify.app/'>TODO-app</a>
-      
-      
+    <div className="container">
+      <h1 className="title">bswlganesh</h1>
+      <p className="subtitle">🚧 This website is under construction 🚧</p>
+
+      <Slab color="#2c94e5" size="medium" />
+
+      <p className="work-text">Check out my work:</p>
+
+      <div className="links">
+        <a href="https://filminfosis.netlify.app/" target="_blank" rel="noopener noreferrer">
+          🎬 Filminfosis
+        </a>
+        <a href="https://bswl-todolist.netlify.app/" target="_blank" rel="noopener noreferrer">
+          ✅ TODO App
+        </a>
+      </div>
     </div>
-  )
+  );
 }
